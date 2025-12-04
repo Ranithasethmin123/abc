b10a8458dfd47951ff9e0cd1f369e47a68e01535
Ranithasethmin123 <ranithasethmin2@gmail.com>
2025-12-04 14:12:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '3e9a205a373ae30a' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'a6a92a5fe7c6c36e' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
